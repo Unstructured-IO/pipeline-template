32ddd005f176c1e946ed41919261026d74623fcb
natygyoon <ygyoon@uwaterloo.ca>
2023-04-10 15:51:55 -0400

    make generate-api

--- {{ cookiecutter.repo_name }}/prepline_{{ cookiecutter.pipeline_package }}/api/app.py
+++ {{ cookiecutter.repo_name }}/prepline_{{ cookiecutter.pipeline_package }}/api/app.py
@@ -14,12 +14,13 @@ app = FastAPI(
     title="Unstructured Pipeline API",
     description="""""",
     version="1.0.0",
+    docs_url="/{{ cookiecutter.pipeline_family }}/docs",
 )
 
 app.include_router(process_file_router)
 app.include_router(process_text_router)
 
 
-@app.get("/healthcheck", status_code=status.HTTP_200_OK)
-async def healthcheck(request: Request):
+@app.get("/healthcheck", status_code=status.HTTP_200_OK, include_in_schema=False)
+def healthcheck(request: Request):
     return {"healthcheck": "HEALTHCHECK STATUS: EVERYTHING OK!"}
--- {{ cookiecutter.repo_name }}/prepline_{{ cookiecutter.pipeline_package }}/api/process_file.py
+++ {{ cookiecutter.repo_name }}/prepline_{{ cookiecutter.pipeline_package }}/api/process_file.py
@@ -3,12 +3,25 @@
 # DO NOT MODIFY DIRECTLY
 #####################################################################
 
+import io
 import os
+import gzip
+import mimetypes
 from typing import List, Union
-from fastapi import status, FastAPI, File, Form, Request, UploadFile, APIRouter
+from fastapi import (
+    status,
+    FastAPI,
+    File,
+    Form,
+    Request,
+    UploadFile,
+    APIRouter,
+    HTTPException,
+)
 from fastapi.responses import PlainTextResponse
 import json
 from fastapi.responses import StreamingResponse
+from starlette.datastructures import Headers
 from starlette.types import Send
 from base64 import b64encode
 from typing import Optional, Mapping, Iterator, Tuple
@@ -27,6 +40,36 @@ def pipeline_api(
     m_some_parameters=[],
 ):
     return f"{':'.join([filename, file_content_type, str(len(file.read()))])}"
+
+
+def get_validated_mimetype(file):
+    """
+    Return a file's mimetype, either via the file.content_type or the mimetypes lib if that's too
+    generic. If the user has set UNSTRUCTURED_ALLOWED_MIMETYPES, validate against this list and
+    return HTTP 400 for an invalid type.
+    """
+    content_type = file.content_type
+    if not content_type or content_type == "application/octet-stream":
+        content_type = mimetypes.guess_type(str(file.filename))[0]
+
+        # Markdown mimetype is too new for the library - just hardcode that one in for now
+        if not content_type and ".md" in file.filename:
+            content_type = "text/markdown"
+
+    allowed_mimetypes_str = os.environ.get("UNSTRUCTURED_ALLOWED_MIMETYPES")
+    if allowed_mimetypes_str is not None:
+        allowed_mimetypes = allowed_mimetypes_str.split(",")
+
+        if content_type not in allowed_mimetypes:
+            raise HTTPException(
+                status_code=400,
+                detail=(
+                    f"Unable to process {file.filename}: "
+                    f"File type {content_type} is not supported."
+                ),
+            )
+
+    return content_type
 
 
 class MultipartMixedResponse(StreamingResponse):
@@ -87,12 +130,29 @@ class MultipartMixedResponse(StreamingResponse):
         await send({"type": "http.response.body", "body": b"", "more_body": False})
 
 
-@router.post("/{ cookiecutter.pipeline_family }}/v0.0.1/process-file")
-async def pipeline_1(
+def ungz_file(file: UploadFile) -> UploadFile:
+    filename = str(file.filename) if file.filename else ""
+    gzip_file = gzip.open(file.file)
+    return UploadFile(
+        file=io.BytesIO(gzip_file.read()),
+        size=len(gzip_file.read()),
+        filename=filename[:-3] if len(filename) > 3 else "",
+        headers=Headers({"content-type": str(mimetypes.guess_type(filename)[0])}),
+    )
+
+
+@router.post("/{{ cookiecutter.pipeline_family }}/v0/process-file")
+@router.post("/{{ cookiecutter.pipeline_family }}/v0.0.1/process-file")
+def pipeline_1(
     request: Request,
     files: Union[List[UploadFile], None] = File(default=None),
     some_parameters: List[str] = Form(default=[]),
 ):
+    if files:
+        for file_index in range(len(files)):
+            if files[file_index].content_type == "application/gzip":
+                files[file_index] = ungz_file(files[file_index])
+
     content_type = request.headers.get("Accept")
 
     if isinstance(files, list) and len(files):
@@ -112,13 +172,15 @@ async def pipeline_1(
 
             def response_generator(is_multipart):
                 for file in files:
+                    file_content_type = get_validated_mimetype(file)
+
                     _file = file.file
 
                     response = pipeline_api(
                         _file,
                         m_some_parameters=some_parameters,
                         filename=file.filename,
-                        file_content_type=file.content_type,
+                        file_content_type=file_content_type,
                     )
                     if is_multipart:
                         if type(response) not in [str, bytes]:
@@ -135,11 +197,13 @@ async def pipeline_1(
             file = files[0]
             _file = file.file
 
+            file_content_type = get_validated_mimetype(file)
+
             response = pipeline_api(
                 _file,
                 m_some_parameters=some_parameters,
                 filename=file.filename,
-                file_content_type=file.content_type,
+                file_content_type=file_content_type,
             )
 
             return response
@@ -149,11 +213,6 @@ async def pipeline_1(
             content='Request parameter "files" is required.\n',
             status_code=status.HTTP_400_BAD_REQUEST,
         )
-
-
-@app.get("/healthcheck", status_code=status.HTTP_200_OK)
-async def healthcheck(request: Request):
-    return {"healthcheck": "HEALTHCHECK STATUS: EVERYTHING OK!"}
 
 
 app.include_router(router)
